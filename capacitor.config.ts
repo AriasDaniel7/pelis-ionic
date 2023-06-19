f7396daced5d699e2e0e6e6a70ad4aaa600f1ea8
Daniel Arias <118637614+AriasDaniel7@users.noreply.github.com>
2023-06-19 13:31:07 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pelis',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
